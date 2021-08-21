cc8aac694e3c2c46ae3fb216dc702591811af87d
Ying <cy240705@gmail.com>
2021-08-20 21:56:41 -0700

    p

--- window_slicing/window_slicing.py
+++ window_slicing/window_slicing.py
@@ -42,4 +42,69 @@
             else:
                 charSet.remove(s[left])
                 left+=1
-        return result 
+        return result
+        
+
+# 76.Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+
+# The testcases will be generated such that the answer is unique.
+
+# A substring is a contiguous sequence of characters within the string.
+
+ 
+
+# Example 1:
+
+# Input: s = "ADOBECODEBANC", t = "ABC"
+# Output: "BANC"
+# Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+# Example 2:
+
+# Input: s = "a", t = "a"
+# Output: "a"
+# Explanation: The entire string s is the minimum window.
+# Example 3:
+
+# Input: s = "a", t = "aa"
+# Output: ""
+# Explanation: Both 'a's from t must be included in the window.
+# Since the largest window of s only has one 'a', return empty string.
+ 
+
+# Constraints:
+
+# m == s.length
+# n == t.length
+# 1 <= m, n <= 105
+# s and t consist of uppercase and lowercase English letters.
+
+    def minWindow(self, s: str, t: str) -> str:
+        window, countT = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        l = 0
+        minLen = float('inf')
+        minWin = [-1,-1]
+        need = len(countT)
+        have = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+            
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                if r - l + 1 < minLen:
+                    minLen = r-l+1
+                    minWin = [l,r]
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        if minWin[0] == -1:
+            return ""
+        else:
+            return s[minWin[0]:minWin[1]+1]
+        
