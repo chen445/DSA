01f77a195d23f03b34dd2b2e23af8b73d615097e
Ying <cy240705@gmail.com>
2021-08-20 21:57:59 -0700

    p

--- window_slicing/window_slicing.py
+++ window_slicing/window_slicing.py
@@ -107,4 +107,60 @@
             return ""
         else:
             return s[minWin[0]:minWin[1]+1]
-        
+# 239.You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+# Return the max sliding window.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+# Example 3:
+
+# Input: nums = [1,-1], k = 1
+# Output: [1,-1]
+# Example 4:
+
+# Input: nums = [9,11], k = 2
+# Output: [11]
+# Example 5:
+
+# Input: nums = [4,-2], k = 2
+# Output: [4]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# 1 <= k <= nums.length
+
+
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        new_arr=[]
+        max_num=deque()
+        for i,n in enumerate(nums):
+            while max_num and nums[max_num[-1]]<=n:
+                    max_num.pop()
+            max_num +=[i]
+            if i-max_num[0]>=k:
+                max_num.popleft()
+            if i+1>=k:
+                new_arr.append(nums[max_num[0]])
+        return new_arr
+
